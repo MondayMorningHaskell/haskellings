c461377425d7d60271eb7e08f639b7995ad2b8bf
James Bowen <jhb563@users.noreply.github.com>
2021-02-15 15:27:01 -0800

    Add unit tests for directory functions (#4)

--- src/DirectoryUtils.hs
+++ src/DirectoryUtils.hs
@@ -11,18 +11,31 @@ isWindows :: Bool
 isWindows = os `notElem` ["linux", "unix", "darwin"]
 
 basename :: FilePath -> FilePath
-basename = takeWhileEnd (\c -> c /= '/' && c /= '\\')
+basename fp = takeWhileEnd (\c -> c /= '/' && c /= '\\') trimmedFp
+  where
+    trimmedFp = if isRelativeEnd fp then init fp else fp
 
 -- Like doing "cd .." with this filepath
 dropDirectoryLevel :: FilePath -> FilePath
-dropDirectoryLevel fp = init $ dropWhileEnd (\c -> c /= '/' && c /= '\\') fp
+dropDirectoryLevel fp = init $ dropWhileEnd (\c -> c /= '/' && c /= '\\') trimmedFp
+  where
+    trimmedFp = if isRelativeEnd fp then init fp else fp
 
 makeRelative :: FilePath -> FilePath
-makeRelative path = if isRelativeBegin path
-  then path
-  else if isWindows
-    then '\\' : path
-    else '/' : path
+makeRelative path = if isWindows
+  then makeRelativeWindows path
+  else makeRelativeUnix path
+
+makeRelativeWindows :: FilePath -> FilePath
+makeRelativeWindows = makeRelative' '\\'
+
+makeRelativeUnix :: FilePath -> FilePath
+makeRelativeUnix = makeRelative' '/'
+
+makeRelative' :: Char -> FilePath -> FilePath
+makeRelative' delimiter fp = if head fp == delimiter
+  then fp
+  else delimiter : fp
 
 isRelativeEnd :: FilePath -> Bool
 isRelativeEnd "" = False
@@ -33,9 +46,20 @@ isRelativeBegin "" = False
 isRelativeBegin fp = head fp == '/' || head fp == '\\'
 
 pathJoin :: FilePath -> FilePath -> FilePath
-pathJoin fp1 fp2 = case (isRelativeEnd fp1, isRelativeBegin fp2) of
+pathJoin fp1 fp2 = if isWindows
+  then pathJoinWindows fp1 fp2
+  else pathJoinUnix fp1 fp2
+
+pathJoinUnix :: FilePath -> FilePath -> FilePath
+pathJoinUnix = pathJoin' '/'
+
+pathJoinWindows :: FilePath -> FilePath -> FilePath
+pathJoinWindows = pathJoin' '\\'
+
+pathJoin' :: Char -> FilePath -> FilePath -> FilePath
+pathJoin' delimiter fp1 fp2 = case (isRelativeEnd fp1, isRelativeBegin fp2) of
   (True, True) -> fp1 ++ tail fp2
-  (False, False) -> fp1 ++ makeRelative fp2
+  (False, False) -> fp1 ++ makeRelative' delimiter fp2
   _ -> fp1 ++ fp2
 
 returnIfDirExists :: FilePath -> IO (Maybe FilePath)
