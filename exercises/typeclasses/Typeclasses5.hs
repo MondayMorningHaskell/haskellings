a58d2cf7fa121519f002d29bf7d8dff1b78394b4
James Bowen <jhbowen047@gmail.com>
2021-02-25 08:46:46 -0800

    Fix ex 5

--- exercises/typeclasses/Typeclasses5.hs
+++ exercises/typeclasses/Typeclasses5.hs
@@ -26,7 +26,7 @@ printEquals x y = if x == y -- < Can use (==) because of implicit Eq constraint
 -- TODO:
 
 -- Define a class called Debuggable
--- This class should depend on both 'Show' and 'Read'
+-- This class should depend on 'Eq', 'Show', and 'Read'
 -- It should have two functions:
 
 -- logObject should take the class variable and a string for the file
@@ -40,7 +40,7 @@ printEquals x y = if x == y -- < Can use (==) because of implicit Eq constraint
 data Person = Person String String Int
   deriving (Show, Read, Eq)
 
--- logObject (Person "John" "Smith" 32) "Test.hs" -> "Produced 'Person John Smith 32' from file Test.hs"
+-- logObject (Person "John" "Smith" 32) "Test.hs" -> "Produced 'Person \"John\" \"Smith\" 32' from file Test.hs"
 -- compareFromEntry "Person \"John\" \"Smith\" 32" (Person "John" "Smith" 32)
 --   -> (True, "Found entered Person object equivalent.")
 -- compareFromEntry "Adult \"Jane\" \"Smith\" 32" (Adult "John" "Smith" 32)
@@ -68,22 +68,22 @@ compareAndPrint = undefined
 
 main :: IO ()
 main = defaultMain $ testGroup "Typeclasses5" $
-  [ testCase "Log Object 1" $ logObject (Person "John" "Smith" 32) "Test.hs" @?= "Produced 'Person John Smith 32' from file Test.hs"
-  , testCase "Log Object 2" $ logObject (Person "Jane" "Doe" 31) "Input.txt" @?= "Produced 'Person Jane Doe 31' from file Input.txt"
+  [ testCase "Log Object 1" $ logObject (Person "John" "Smith" 32) "Test.hs" @?= "Produced 'Person \"John\" \"Smith\" 32' from file Test.hs"
+  , testCase "Log Object 2" $ logObject (Person "Jane" "Doe" 31) "Input.txt" @?= "Produced 'Person \"Jane\" \"Doe\" 31' from file Input.txt"
   , testCase "Log Object 3" $ logObject (Point3 3 4 5) "Test.hs" @?= "Calculated 'Point3 3 4 5' from input file Test.hs"
   , testCase "Log Object 4" $ logObject (Point3 6 8 10) "Input.csv" @?= "Calculated 'Point3 6 8 10' from input file Input.csv"
-  , testCase "Compare From Entry 1" $ compareFromEntry "Person John Smith 32" (Person "John" "Smith" 32) @?=
+  , testCase "Compare From Entry 1" $ compareFromEntry "Person \"John\" \"Smith\" 32" (Person "John" "Smith" 32) @?=
       (True, "Found entered Person object equivalent.")
-  , testCase "Compare From Entry 2" $ compareFromEntry "Person John Smith 32" (Person "Jane" "Smith" 35) @?=
+  , testCase "Compare From Entry 2" $ compareFromEntry "Person \"John\" \"Smith\" 32" (Person "Jane" "Smith" 35) @?=
       (False, "Found entered Person object does not match.")
   , testCase "Compare From Entry 3" $ compareFromEntry "Point3 3 4 5" (Point3 3 4 5) @?=
       (True, "New Point calculation matches.")
   , testCase "Compare From Entry 4" $ compareFromEntry "Point3 3 4 5" (Point3 6 8 10) @?=
       (False, "New Point calculation does not match previous.")
-  , testCase "Compare and Print 1" $ compareAndPrint "Person John Smith 32" (Person "John" "Smith" 32) @?=
-      (True, "'Person John Smith 32' vs. 'Person John Smith 32'")
-  , testCase "Compare and Print 2" $ compareAndPrint "Person John Smith 32" (Person "Jane" "Smith" 31) @?=
-      (False, "'Person John Smith 32' vs. 'Person Jane Smith 31'")
+  , testCase "Compare and Print 1" $ compareAndPrint "Person \"John\" \"Smith\" 32" (Person "John" "Smith" 32) @?=
+      (True, "'Person \"John\" \"Smith\" 32' vs. 'Person \"John\" \"Smith\" 32'")
+  , testCase "Compare and Print 2" $ compareAndPrint "Person \"John\" \"Smith\" 32" (Person "Jane" "Smith" 31) @?=
+      (False, "'Person \"John\" \"Smith\" 32' vs. 'Person \"Jane\" \"Smith\" 31'")
   , testCase "Compare and Print 3" $ compareAndPrint "Point3 3 4 5" (Point3 3 4 5) @?=
       (True, "'Point3 3 4 5' vs. 'Point3 3 4 5'")
   , testCase "Compare and Print 4" $ compareAndPrint "Point3 3 4 5" (Point3 6 8 10) @?=
