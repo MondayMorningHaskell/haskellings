566e517d6c96464452d51ef1fcbf3defac0cdeb6
James Bowen <jhbowen047@gmail.com>
2021-02-25 22:29:04 -0800

    Lists 1

--- src/ExerciseList.hs
+++ src/ExerciseList.hs
@@ -44,6 +44,7 @@ allExercises =
   , ExerciseInfo "Recursion2" "recursion" True "1. Sometimes there is no work and only a recursive call! 2. Pattern matches can reveal more than just the head of a list!"
   , ExerciseInfo "Recursion3" "recursion" True "You can use multiple accumulator arguments if you want!"
   , ExerciseInfo "Recursion4" "recursion" True "You can't technically use 'tail recursion' but you'll still want a helper function, at least on the second function!"
+  , ExerciseInfo "Lists1" "lists" True "Tail recursion should provide you with some inspiration for your folding functions!"
   ]
 
 allExercisesMap :: M.Map String ExerciseInfo
