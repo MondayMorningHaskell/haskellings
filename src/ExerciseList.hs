ba019546b15706eac50f564a1695229bcdd6bab0
James Bowen <jhbowen047@gmail.com>
2021-03-01 09:14:55 -0800

    Writer monad exercise

--- src/ExerciseList.hs
+++ src/ExerciseList.hs
@@ -54,6 +54,7 @@ allExercises =
   , ExerciseInfo "Monads1" "monads" True "The bind operator (>>=) allows us to pass values from one monadic function to another without needing to unwrap in between!"
   , ExerciseInfo "Monads2" "monads" True "The key is understanding what goes on each side of '<-' operator. The wrapped computation goes on the right, the unwrapped result goes on the left."
   , ExerciseInfo "Monads3" "monads" True "You can still use an 'if' inside do-syntax! Just make sure that both branches are monadic actions!"
+  , ExerciseInfo "Monads4" "monads" True "You'll have to wrap the string values in their own lists to use the monoid instance!"
   ]
 
 allExercisesMap :: M.Map String ExerciseInfo
