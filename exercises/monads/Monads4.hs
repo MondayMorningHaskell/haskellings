b1884baed26865c684a855b137cc7346deea8d69
James Bowen <jhbowen047@gmail.com>
2021-03-01 18:35:58 -0800

    Fix Writer exercise

--- exercises/monads/Monads4.hs
+++ exercises/monads/Monads4.hs
@@ -1,5 +1,6 @@
 -- I AM NOT DONE
 
+import Control.Monad.Writer
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -69,7 +70,7 @@ applyOpCount :: Op -> Double -> Writer IntAdd Double
 applyOpCount = undefined
 
 -- Now apply a series of operations to an input value using the 'Writer' monad!
-applyAndCountOperations :: [Op] -> Double -> (Double, Int)
+applyAndCountOperations :: [Op] -> Double -> (Double, IntAdd)
 applyAndCountOperations = undefined
 
 -- Do the same as above, except instead of counting the cost, log the
@@ -86,23 +87,23 @@ opList1 :: [Op]
 opList1 = [Add 5.0, Subtract 2.0, Multiply 3.0, Divide 2.0]
 
 opList2 :: [Op]
-opList2 = [Multiply 2.0, Add 5.0, Sqrt]
+opList2 = [Multiply 2.0, Sqrt]
 
 main :: IO ()
 main = defaultMain $ testGroup "Monads4" $
-  [ testCase "applyOpLog 1" $ runWriter (applyOpLog (Add 1) 5.0) @?= (6.0, IntAdd 1)
-  , testCase "applyOpLog 2" $ runWriter (applyOpLog (Multiply 2) 5.0) @?= (10.0, IntAdd 5)
-  , testCase "applyOpLog 3" $ runWriter (applyOpLog (Divide 2) 6.0) @?= (3.0, IntAdd 10)
-  , testCase "applyOpLog 4" $ runWriter (applyOpLog Sqrt 4.0) @?= (2.0, IntAdd 20)
+  [ testCase "applyOpCount 1" $ runWriter (applyOpCount (Add 1) 5.0) @?= (6.0, IntAdd 1)
+  , testCase "applyOpCount 2" $ runWriter (applyOpCount (Multiply 2) 5.0) @?= (10.0, IntAdd 5)
+  , testCase "applyOpCount 3" $ runWriter (applyOpCount (Divide 2) 6.0) @?= (3.0, IntAdd 10)
+  , testCase "applyOpCount 4" $ runWriter (applyOpCount Sqrt 4.0) @?= (2.0, IntAdd 20)
   , testCase "applyAndCountOperations 1" $ applyAndCountOperations [] 5.0 @?= (5.0, IntAdd 0)
   , testCase "applyAndCountOperations 2" $ applyAndCountOperations [Add 1.0] 5.0 @?= (6.0, IntAdd 1)
   , testCase "applyAndCountOperations 3" $ applyAndCountOperations [Subtract 3.0] 8.0 @?= (5.0, IntAdd 2)
   , testCase "applyAndCountOperations 4" $ applyAndCountOperations opList1 13.0 @?= (24.0, IntAdd 18)
   , testCase "applyAndCountOperations 5" $ applyAndCountOperations opList2 8.0 @?= (4.0, IntAdd 25)
-  , testCase "applyOpCount 1" $ runWriter (applyOpCount (Add 1) 5.0) @?= (6.0, ["Adding 1"])
-  , testCase "applyOpCount 2" $ runWriter (applyOpCount (Multiply 2) 5.0) @?= (10.0, ["Multiplying by 2"])
-  , testCase "applyOpCount 3" $ runWriter (applyOpCount (Divide 2) 6.0) @?= (3.0, ["Dividing by 2"])
-  , testCase "applyOpCount 4" $ runWriter (applyOpCount Sqrt 4.0) @?= (2.0, ["Taking Square Root"])
+  , testCase "applyOpLog 1" $ runWriter (applyOpLog (Add 1) 5.0) @?= (6.0, ["Adding 1.0"])
+  , testCase "applyOpLog 2" $ runWriter (applyOpLog (Multiply 2) 5.0) @?= (10.0, ["Multiplying by 2.0"])
+  , testCase "applyOpLog 3" $ runWriter (applyOpLog (Divide 2) 6.0) @?= (3.0, ["Dividing by 2.0"])
+  , testCase "applyOpLog 4" $ runWriter (applyOpLog Sqrt 4.0) @?= (2.0, ["Taking Square Root"])
   , testCase "applyAndLogOperations 1" $ applyAndLogOperations [] 5.0 @?= (5.0, [])
   , testCase "applyAndLogOperations 2" $ applyAndLogOperations [Add 1.0] 5.0 @?= (6.0, ["Adding 1.0"])
   , testCase "applyAndLogOperations 3" $ applyAndLogOperations [Subtract 3.0] 8.0 @?= (5.0, ["Subtracting 3.0"])
