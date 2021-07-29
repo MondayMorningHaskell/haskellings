46c371ae0953a273af0a3033570408fe4c91e27f
James Bowen <jhbowen047@gmail.com>
2021-07-28 17:19:23 -0700

    Update versions and package commit for nix files

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/34f85de51bbc74595e63b22ee089adbb31f7c7a2.tar.gz") {}, compiler ? "ghc884" }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz") {}, compiler ? "ghc8104" }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [ 
-    haskell.compiler.ghc884
+    haskell.compiler.ghc8104
     (import ./default.nix { inherit pkgs compiler; })
   ];
 
