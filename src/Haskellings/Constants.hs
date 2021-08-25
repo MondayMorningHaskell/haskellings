08ba91a99ec777f3131ea660f142680f471ebfcd
James Bowen <jhbowen047@gmail.com>
2021-08-24 22:51:53 -0700

    Haddock for constants and directory utils

--- src/Haskellings/Constants.hs
+++ src/Haskellings/Constants.hs
@@ -1,55 +1,83 @@
-{- Constants used throughout the program,
-   often related to configuration.
+{-|
+Module      : Haskellings.Constants
+Description : Constant expressions for Haskellings
+License     : BSD3
+Maintainer  : james@mondaymorninghaskell.me
+
+This module has constant values that are used across Haskellings.
+It includes things like the string for the GHC version currently in use,
+hard-coded directory and file names, and required libraries.
 -}
-module Haskellings.Constants (
-  ghcVersion,
-  ghcVersionNumber,
-  projectRootDirName,
-  configFileName,
-  envIsCi,
-  ciProjectRootDirName,
-  haskellingsVersion,
-  mainProjectExercisesDir,
-  haskellingsRequiredLibs
+module Haskellings.Constants
+( -- * Version strings
+  -- | Includes the GHC version currently in use, and the Haskellings version.
+    ghcVersion
+  , ghcVersionNumber
+  , haskellingsVersion
+  -- * Files and directories
+  -- | These help us locate the exercises and configurations.
+  , projectRootDirName
+  , mainProjectExercisesDir
+  , configFileName
+  -- * Required Libraries
+  -- | Haskellings requires these are installed with the Stack package
+  --   in order to function.
+  , haskellingsRequiredLibs
+  -- * CI Constants
+  -- | These help us to run tests in the Circle CI environment.
+  , envIsCi
+  , ciProjectRootDirName
 ) where
 
 import           Data.Maybe
 import           System.Environment
 
+-- | The GHC version currently used by Haskellings. We use this
+--   to locate the appropriate GHC executable.
 ghcVersion :: String
 ghcVersion = "ghc-8.10.4"
 
+-- | The version number, isolated from any prefix. Also helps in
+--   finding certain directories.
 ghcVersionNumber :: String
 ghcVersionNumber = "8.10.4"
 
-projectRootDirName :: String
-projectRootDirName = "haskellings"
-
-configFileName :: String
-configFileName = "config.yaml"
-
-envIsCi :: IO Bool
-envIsCi = isJust <$> lookupEnv ciEnvName
-
-ciProjectRootDirName :: String
-ciProjectRootDirName = "project"
-
+-- | The current Haskellings program version.
 haskellingsVersion :: String
 haskellingsVersion = "0.8.0.0"
 
+-- | The project root directory name. We need to find the project root
+--   in order to locate the exercises.
+projectRootDirName :: String
+projectRootDirName = "haskellings"
+
+-- | The name of the exercises directory.
 mainProjectExercisesDir :: String
 mainProjectExercisesDir = "exercises"
 
--- A listing of packages required by exercises, so we can use them
--- to filter Stack snapshots
+-- | The name of the config file, which can store a custom GHC location.
+configFileName :: String
+configFileName = "config.yaml"
+
+-- | A listing of packages required by exercises, so we can use them
+--   to filter Stack snapshots
 haskellingsRequiredLibs :: [String]
 haskellingsRequiredLibs =
   [ "tasty"
   , "tasty-hunit"
   ]
 
+-- | Tells us if we are running in a Circle CI environment. This affects
+--   how we construct the path and find the exercises.
+envIsCi :: IO Bool
+envIsCi = isJust <$> lookupEnv ciEnvName
+
+-- | The project root name when running on Circle CI.
+ciProjectRootDirName :: String
+ciProjectRootDirName = "project"
+
 ---------- PRIVATE FUNCTIONS ----------
 
--- On CircleCI, the root directory shows up as "project'
+-- We set this environment variable on Circle CI.
 ciEnvName :: String
 ciEnvName = "HASKELLINGS_CI_ENV"
