40e4a833fc7840707cea10031f31ab4781e81589
James Bowen <jhbowen047@gmail.com>
2021-08-25 20:40:54 -0700

    Haddock for run commands and terminal utils

--- src/Haskellings/RunCommands.hs
+++ src/Haskellings/RunCommands.hs
@@ -1,12 +1,22 @@
-{- Core wrapper functions for each commands,
-   e.g. 'run', 'exec', 'hint', 'watch', 'list
+{-|
+Module      : Haskellings.RunCommands
+Description : Core wrapper functions for each command
+License     : BSD3
+Maintainer  : james@mondaymorninghaskell.me
+
+The are the primary functions called for each command, including
+'run', 'exec', 'hint', and 'list' (but not including 'watch').
 -}
+
 module Haskellings.RunCommands (
+  -- * Running Exercises
   runExercise,
   execExercise,
-  hintExercise,
+  -- * Listing Exercises
   listExercises,
   listExercises',
+  -- * Helper Commands
+  hintExercise,
   runHelp,
   runConfigure
 ) where
@@ -26,26 +36,26 @@ import           Haskellings.Processor
 import           Haskellings.TerminalUtils
 import           Haskellings.Types
 
+-- | Run the exercise given by the string (i.e. 'haskellings run')
 runExercise :: String -> ReaderT ProgramConfig IO ()
 runExercise exerciseName = case M.lookup exerciseName allExercisesMap of
   Nothing -> progPutStrLn $ "Could not find exercise: " ++ exerciseName ++ "!"
   Just exInfo -> compileAndRunExercise_ exInfo
 
+-- | Execute the exercise given by the string (i.e. 'haskellings exec')
+--   Only used with executable exercises.
 execExercise :: String -> ReaderT ProgramConfig IO ()
 execExercise exerciseName = case M.lookup exerciseName allExercisesMap of
   Nothing -> progPutStrLn $ "Could not find exercise: " ++ exerciseName ++ "!"
   Just exInfo@(ExerciseInfo _ _ (Executable _ _) _) -> executeExercise exInfo
   _ -> progPutStrLn $ "Exercise " ++ exerciseName ++ " is not executable!"
 
-hintExercise :: String -> ReaderT ProgramConfig IO ()
-hintExercise exerciseName = case M.lookup exerciseName allExercisesMap of
-  Nothing -> progPutStrLn $ "Could not find exercise: " ++ exerciseName ++ "!"
-  Just exInfo -> progPutStrLn (exerciseHint exInfo)
-
+-- | List all the exercises in Haskellings with their status (done/not done).
+--   (i.e. 'haskellings list')
 listExercises :: ReaderT ProgramConfig IO ()
 listExercises = listExercises' allExercises
 
--- Separated for testability
+-- | Core logic function for listing. Separated for testing.
 listExercises' :: [ExerciseInfo] -> ReaderT ProgramConfig IO ()
 listExercises' [] = progPutStrLn "No exercises!"
 listExercises' exercises = do
@@ -67,6 +77,13 @@ listExercises' exercises = do
       then withTerminalFailure $ printNameAndDots >> progPutStrLn "...NOT DONE"
       else withTerminalSuccess $ printNameAndDots >> progPutStrLn ".......DONE"
 
+-- | Give a hint for the given exercise (i.e. 'haskellings hint')
+hintExercise :: String -> ReaderT ProgramConfig IO ()
+hintExercise exerciseName = case M.lookup exerciseName allExercisesMap of
+  Nothing -> progPutStrLn $ "Could not find exercise: " ++ exerciseName ++ "!"
+  Just exInfo -> progPutStrLn (exerciseHint exInfo)
+
+-- | List available commands (i.e. 'haskellings help')
 runHelp :: IO ()
 runHelp = mapM_ putStrLn
   [ "Available commands:"
@@ -79,6 +96,8 @@ runHelp = mapM_ putStrLn
   , "  haskellings help (-h, --help) -- Display this help message."
   ]
 
+-- | Configure the project. Allows the user to set the GHC path and
+--   the Stack package path, saving them in a config file.
 runConfigure :: IO ()
 runConfigure = do
   projectRoot' <- findProjectRoot
--- src/Haskellings/TerminalUtils.hs
+++ src/Haskellings/TerminalUtils.hs
@@ -1,13 +1,23 @@
-{- Utility functions for printing to the terminal.
-   Deal with different kinds of Handles, adds color.
+{-|
+Module      : Haskellings.TerminalUtils
+Description : Utility functions for printing to the terminal.
+License     : BSD3
+Maintainer  : james@mondaymorninghaskell.me
+
+These functions allow printing to the terminal, but use a ProgramConfig
+so that test functions can use files for input and output. Also allows
+coloring terminal output to be green or red.
 -}
+
 module Haskellings.TerminalUtils (
+  -- * Basic Printing with Program Config
   progPutStr,
   progPutStrLn,
   progPrint,
   progPutStrErr,
   progPrintErr,
   progReadLine,
+  -- * Success and Failure Functions
   withTerminalSuccess,
   withTerminalFailure,
   progPutStrLnSuccess,
@@ -21,47 +31,53 @@ import           System.IO
 
 import           Haskellings.Types
 
+-- | Print the given string without a new line.
 progPutStr :: String -> ReaderT ProgramConfig IO ()
 progPutStr str = do
   handle <- asks outHandle
   lift $ hPutStr handle str
 
+-- | Print the given string with a new line.
 progPutStrLn :: String -> ReaderT ProgramConfig IO ()
 progPutStrLn str = do
   handle <- asks outHandle
   lift $ hPutStrLn handle str
 
+-- | Print the Show-able object with a new line.
 progPrint :: (Show a) => a -> ReaderT ProgramConfig IO ()
 progPrint val = do
   handle <- asks outHandle
   lift $ hPrint handle val
 
+-- | Print the given string (with a new line) to the error stream.
 progPutStrErr :: String -> ReaderT ProgramConfig IO ()
 progPutStrErr str = do
   handle <- asks errHandle
   lift $ hPutStrLn handle str
 
+-- | Print the Show-able object (with a new line) to the error stream.
 progPrintErr :: (Show a) => a -> ReaderT ProgramConfig IO ()
 progPrintErr val = do
   handle <- asks errHandle
   lift $ hPrint handle val
 
+-- | Read a line using the Program Config's input handler.
 progReadLine :: ReaderT ProgramConfig IO String
 progReadLine = asks inHandle >>= (lift . hGetLine)
 
--- Perform an action with 'Green' Terminal Text
+-- | Perform an action with 'Green' terminal text.
 withTerminalSuccess :: (MonadIO m) => m a -> m a
 withTerminalSuccess = withTerminalColor Green
 
--- Perform an action with 'Red' Terminal Text
+-- | Perform an action with 'Red' terminal text.
 withTerminalFailure :: (MonadIO m) => m a -> m a
 withTerminalFailure = withTerminalColor Red
 
--- Print a line, but in Green
+-- | Print a line, but with Green color.
 progPutStrLnSuccess :: String -> ReaderT ProgramConfig IO ()
 progPutStrLnSuccess output = withTerminalSuccess (progPutStrLn output)
 
--- Print a line, but in Red
+-- | Print a line, but with Red color.
 progPutStrLnFailure :: String -> ReaderT ProgramConfig IO ()
 progPutStrLnFailure output = withTerminalFailure (progPutStrLn output)
 
